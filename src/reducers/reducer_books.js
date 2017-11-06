0fb50a23c524102dad244af3f93356c48c0335a6
KirillPolianin <k.polianin@gmail.com>
2017-11-07 00:42:07 +0200

    Simple react-redux app is ready and working

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
-	return [
-		{ title: 'Javascript: The Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Eloquent Ruby' }
-	];
+  return [
+    { title: 'Javascript: The Good Parts', pages: 230 },
+    { title: 'Harry Potter', pages: 554 },
+    { title: 'The Dark Tower', pages: 135 },
+    { title: 'Eloquent Ruby', pages: 81 }
+  ];
 }
